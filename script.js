30b44c9fbde0a2ef6e618ef3de1cb5876ba16619
shravanighag055-dev <shravanighag055@gmail.com>
2026-06-14 12:48:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ document.getElementById("result").innerHTML =
 "You searched for: " + stock;
 
 }
+function calculatePortfolio(){
+
+let buyPrice =
+parseFloat(document.getElementById("buyPrice").value);
+
+let currentPrice =
+parseFloat(document.getElementById("currentPrice").value);
+
+let quantity =
+parseInt(document.getElementById("quantity").value);
+
+let investment =
+buyPrice * quantity;
+
+let currentValue =
+currentPrice * quantity;
+
+let profit =
+currentValue - investment;
+
+let returnPercent =
+((profit / investment) * 100).toFixed(2);
+
+document.getElementById("portfolioResult").innerHTML =
+`
+<h3>Results</h3>
+<p>Investment: ₹${investment}</p>
+<p>Current Value: ₹${currentValue}</p>
+<p>Profit/Loss: ₹${profit}</p>
+<p>Return: ${returnPercent}%</p>
+`;
+
+}
